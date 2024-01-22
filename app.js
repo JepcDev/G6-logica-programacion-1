98389ad23bd26fc76f0595c8898365f7e2dab995
JepcDev <joseenrique.pc21@gmail.com>
2024-01-21 21:53:01 -0400

    ADD: funcion promp para el usuario

---
+++ app.js
@@ -0,0 +1,5 @@
+// funcio nativa para mostrar un mensaje pop pup
+alert('Hola Mundo');
+
+// Funcion o metodo SPermite perdirle o preguntar informacion al usuario
+let numeroUsuario = prompt("Me indicas un numero porfavor: ");
