b14218abce2ef145c404e46b225c26da24e297e9
JepcDev <joseenrique.pc21@gmail.com>
2024-01-22 22:30:14 -0400

    ADD: desafio condicion para obtener la licencia

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // funcio nativa para mostrar un mensaje pop pup
 // alert('Hola Mundo');
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
 
 // Funcion o metodo SPermite perdirle o preguntar informacion al usuario
 let numeroUsuario = prompt("Me indicas un numero porfavor: ");
 let numeroSecreto = 6;
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 100;
+let mensajeDeError  = "¡Error! Completa todos los campos";
+
+alert("¡Error! Completa todos los campos");
+alert(mensajeDeError);
+
+nombre = prompt("Por favor ingrese su nombre: ");
+edad = prompt("Por favor ingrese su edad: ");
 
 // Funcion que impreime en consola el valor de variables f12
 // sin las comillas la consola entiende que tiene que extraer el valor de la variable y mostrarlo
 console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-  alert("Acertaste el numero");
+if (edad >= 18) {
+  alert("¡Puedes obtener tu licencia de conducir!");
 } else {
   
 }
