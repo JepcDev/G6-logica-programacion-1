e1824b288aecd99b0349e4b779576f5c1e8e09e4
JepcDev <joseenrique.pc21@gmail.com>
2024-03-05 19:38:32 -0400

    ADD: comentarios y else

--- app.js
+++ app.js
@@ -5,24 +5,32 @@ alert("¡Bienvenida y bievenido a nuestro sitio web!");
 // Funcion o metodo SPermite perdirle o preguntar informacion al usuario
 let numeroUsuario = prompt("Me indicas un numero porfavor: ");
 let numeroSecreto = 6;
-let nombre = "Luna";
-let edad = 25;
-let numeroDeVentas = 50;
-let saldoDisponible = 100;
-let mensajeDeError  = "¡Error! Completa todos los campos";
+// let nombre = "Luna";
+// let edad = 25;
+// let numeroDeVentas = 50;
+// let saldoDisponible = 100;
+// let mensajeDeError  = "¡Error! Completa todos los campos";
 
-alert("¡Error! Completa todos los campos");
-alert(mensajeDeError);
+// alert("¡Error! Completa todos los campos");
+// alert(mensajeDeError);
 
-nombre = prompt("Por favor ingrese su nombre: ");
-edad = prompt("Por favor ingrese su edad: ");
+// nombre = prompt("Por favor ingrese su nombre: ");
+// edad = prompt("Por favor ingrese su edad: ");
 
 // Funcion que impreime en consola el valor de variables f12
 // sin las comillas la consola entiende que tiene que extraer el valor de la variable y mostrarlo
 console.log(numeroUsuario);
 
-if (edad >= 18) {
-  alert("¡Puedes obtener tu licencia de conducir!");
-} else {
-  
+// Realiza la comparacion
+if (numeroUsuario == numeroSecreto) {
+  // Acertamos fue verdadera la condición
+  alert('Acertaste el número');
+}else{
+  // La condición no se cumplió
+  alert('Lo siento, no acertaste el número');
 }
+
+// if (edad >= 18) {
+//   alert("¡Puedes obtener tu licencia de conducir!");
+// } else {
+// }
