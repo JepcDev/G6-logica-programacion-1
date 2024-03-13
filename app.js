6af50bcbd234cafe072646d0a55a2429d88a2343
JepcDev <joseenrique.pc21@gmail.com>
2024-03-12 22:04:36 -0400

    ADD: queryselector manipulando el dom

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// Permite acceder a cada uno de los elementos del DOM, en este caso el selector que usaremos para acceder es h1
+// En este caso nos retornaria el elemento h1 que tenemos en el index.html, que seria el titulo
+// El titulo se convierte en objeto ya que lo que entrega el queryselector es un objeto con muchos metodos para modificarlo
+let titulo = document.querySelector('h1');
+// inserta el texto en el objeto titulo del selector h1
+titulo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número del 1 al 10';
